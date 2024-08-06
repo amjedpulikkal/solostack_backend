e939cb15c4ce3fd7e25bd1635e3f769de4fe932c
amjedpulikkal <amjed0pulikkal@gmail.com>
2024-08-06 16:11:50 +0530

    updated

--- src/infrastructure/server/config/db.ts
+++ src/infrastructure/server/config/db.ts
@@ -1,16 +1,25 @@
-import mongoose from "mongoose";
-require("dotenv").config();
+import mongoose, { ConnectOptions } from "mongoose";
+import dotenv from "dotenv";
 
-const db_url: string = process.env.db || ""
+dotenv.config();
+
+const db_url: string = process.env.db
 
 export async function dbConnect() {
     try {
-        const data = await mongoose.connect(db_url,{dbName:"solostack"})
-        console.log(data.connection.host)
-    } catch (error: any) {
-        console.log(error)
-        
-        setTimeout(dbConnect,5000)
-    }
+        const options: ConnectOptions = {
+            dbName: "solostack",
+            writeConcern: {
+                w: 'majority',
+                wtimeoutMS: 5000  
+            }
+        };
 
+        const data = await mongoose.connect(db_url, options);
+        console.log(`Connected to MongoDB Atlas at ${data.connection.host}`);
+    } catch (error: any) {
+        console.error('Failed to connect to MongoDB Atlas:', error);
+        setTimeout(dbConnect, 5000); 
+    }
 }
+
